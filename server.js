cfd79a058ad3da573bd533ca7e0ca60f7859fd9e
Aryan Pandey <margiaryan@gmail.com>
2023-12-22 10:44:11 +0530

    changed port

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const connectDB = require('./config/db');
-
 const express = require('express');
 const cors = require('cors');
 const globalErrorHandler = require('./middlewares/globalErrorHandler');
-
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 const corsOption = {
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:3000'
 };
 
 app.use(cors(corsOption));
-
 app.use(express.json());
-//app.use("/api", routes);
 connectDB();
 
-const port = 3000;
+const port = 4000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
