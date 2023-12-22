6079c44e8f612ef864f78593b47fc13ab2bd3c56
Aryan Pandey <margiaryan@gmail.com>
2023-12-22 14:58:56 +0530

    feat: otp

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const { signup, login, uploadFile } = require('../controllers/authControllers');
+const User = require('../models/userSchema');
+const otpGenerator = require('otp-generator');
+const { signup, login, uploadFile, protect } = require('../controllers/authControllers');
+const { sendOTPMail } = require('../utils/sendMail');
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -9,21 +12,62 @@ router.get('/', (req, res) => {
 });
 
 // Route to approve a user by a secretary
-router.post('/approve-user/:userId', async (req, res) => {
+router.post('/approve-user/:userId', protect, async (req, res) => {
+  // userId is of user that is need to be approved
   const { userId } = req.params;
-
+  //here user is either secretary or admin
+  const { user } = req.user;
   try {
     // Check if the user has the role of 'secretary'
     if (req.user.role !== 'secretary') {
       return res.status(403).json({ message: 'Only secretaries can approve users' });
     }
-
-    // Update the user's status to 'approved' in the database
-    const updatedUser = await User.findByIdAndUpdate(userId, { status: 'approved' }, { new: true });
-
-    res.json({ message: 'User approved successfully', user: updatedUser });
+    const otp = otpGenerator.generate(6, {
+      lowerCaseAlphabets: false,
+      upperCaseAlphabets: false,
+      specialChars: false
+    });
+    const mailResponse = await sendOTPMail(
+      user.email,
+      'Verification Email',
+      `<h1>Please confirm your OTP</h1>
+       <p>Here is your OTP code: ${otp}</p>`
+    );
+    console.log('Email sent successfully: ', mailResponse);
+    // Save the OTP and its expiration time in the secretary document
+    const otpExpiration = new Date(Date.now() + 2 * 60 * 1000); // 2 minutes expiration
+    await User.findByIdAndUpdate(user._id, { $set: { otp, otpExpiration } });
+    res.json({ message: 'OTP sent to secretary for confirmation' });
   } catch (error) {
     console.error('Error approving user:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Route for confirming OTP entered by secretary
+router.post('/confirm-otp/:userId', protect, async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { user } = req.user;
+    const { enteredOTP } = req.body;
+
+    // Retrieve the secretary's OTP and its expiration time
+    const storedOTP = user.otp;
+    const otpExpiration = user.otpExpiration;
+
+    // Check if the entered OTP matches the stored OTP and is not expired
+    if (enteredOTP === storedOTP && new Date() < new Date(otpExpiration)) {
+      // remove the otp from secretary fields
+      const secretary = await User.findByIdAndUpdate(user._id, { $unset: { otp: '', otpExpiration: '' } });
+      // If the OTP is valid, update the user's status to 'approved' in the database
+      const updatedUser = await User.findByIdAndUpdate(userId, { status: 'approved' }, { new: true });
+
+      res.json({ message: 'User approved successfully', user: updatedUser });
+    } else {
+      res.status(400).json({ message: 'Invalid OTP or OTP expired' });
+    }
+  } catch (error) {
+    console.error('Error confirming OTP:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
